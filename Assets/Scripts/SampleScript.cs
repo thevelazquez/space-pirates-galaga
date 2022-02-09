78e24cf150ae84f330d64be8f268be7527249930
April Dean <aprilldean@gmail.com>
2022-02-09 15:06:31 -0500

    to see sample comment

--- Assets/Scripts/SampleScript.cs
+++ Assets/Scripts/SampleScript.cs
@@ -7,6 +7,7 @@ public class SampleScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        ///A COMMENT TO SHOW THE DIFFERENCE
         
     }
 
