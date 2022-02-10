3395d19e8dd3a1d13159ab702ee10d17fbf69a2f
Edgardo Velazquez <edv9298@hotmail.com>
2022-02-10 16:33:27 -0500

    Added player shooting

---
+++ Assets/Scripts/LaserProjectile.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserProjectile : MonoBehaviour
+{
+    float delay = 1f;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(EndLife());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0,speed*Time.deltaTime,0);
+    }
+
+    IEnumerator EndLife() {
+        yield return new WaitForSeconds(delay);
+        Destroy(gameObject);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float playerSpeed;
+    public GameObject laser;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +20,9 @@ public class PlayerController : MonoBehaviour
         }
         if (Input.GetKey(KeyCode.D)) {
             transform.Translate(move(), 0, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            Instantiate(laser, transform.localPosition + new Vector3(0,1.7f,0), Quaternion.identity);
         }
     }
 
