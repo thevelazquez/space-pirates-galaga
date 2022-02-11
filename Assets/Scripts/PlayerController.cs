2c55ccefc3f3117f124a3afd68f56ba7e9eafbfb
April Dean <aprilldean@gmail.com>
2022-02-11 18:55:16 -0500

    made arrow keys work

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -15,10 +15,10 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.A)) {
+        if (Input.GetKey(KeyCode.A) || Input.GetKey("left")) {
             transform.Translate(-move(), 0, 0);
         }
-        if (Input.GetKey(KeyCode.D)) {
+        if (Input.GetKey(KeyCode.D) || Input.GetKey("right")) {
             transform.Translate(move(), 0, 0);
         }
         if (Input.GetKeyDown(KeyCode.Space)) {
