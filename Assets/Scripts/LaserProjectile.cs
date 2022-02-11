5299a56b71431136edf536fed04acc2628df5ed5
April Dean <aprilldean@gmail.com>
2022-02-11 17:56:51 -0500

    imported enemy assets

--- Assets/Scripts/LaserProjectile.cs
+++ Assets/Scripts/LaserProjectile.cs
@@ -6,10 +6,14 @@ public class LaserProjectile : MonoBehaviour
 {
     float delay = 1f;
     public float speed;
+
+    Rigidbody2D rigibody2d;
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(EndLife());
+        rigibody2d = GetComponent<Rigidbody2D>();
+
     }
 
     // Update is called once per frame
@@ -20,6 +24,11 @@ public class LaserProjectile : MonoBehaviour
 
     IEnumerator EndLife() {
         yield return new WaitForSeconds(delay);
+        Destroy(gameObject);
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
         Destroy(gameObject);
     }
 }
